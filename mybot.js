29a2da4fe0a3084e3c1c7444a49b061e0b43a9e5
Justin Bausili <bausilijm@gmail.com>
2021-02-18 00:58:11 -0500

    added quote functionality

--- mybot.js
+++ mybot.js
@@ -1,32 +1,99 @@
-const { prefix, token, randomMessages } = require('./config.json');
+const { prefix, token, randomMessages, quote, sql } = require('./config.json');
+const pg = require('pg');
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-var randomMessageChance = 100; //%
- 
-const randomMessageCheck = (message) => {
+var connectionString = sql;
+var pgClient = new pg.Client(connectionString);
+var numQuotes = 0;
+var randomMessageChance = 8; //%
+
+try {
+pgClient.connect();
+} catch(error) { console.error(error); }
+
+const talk = (txt, msg) => {
+  msg.channel.send(txt);
+}
+
+const randomMessageCheck = (msg) => {
   let random = Math.floor(Math.random() * 100);
   if (random <= randomMessageChance) {
     let msgNo = Math.floor(Math.random() * randomMessages.length);
-    message.channel.send(`${randomMessages[msgNo]}`);
+    msg.channel.send(`${randomMessages[msgNo]}`);
   }
+}
+
+const addQuote = (author, quote, channel) => {
+  pgClient.query(`insert into quotes (author, quote) values ( '${author}', '${quote}' )`);
+  getNumQuotes();
+  talk(`Quote added!`, channel);
+}
+
+const getQuote = (number, channel) => {
+  pgClient.query(`select * from quotes`).then(r => {
+    if (number > r.rowCount || number < 0) {
+      talk(`Quote doesn't exist. There are ${r.rowCount} quote(s) ya dunce.`, channel);
+      return;
+    }
+    else {
+      pgClient.query(`select * from quotes where id=` + number).then(res => {
+        const data = res.rows;
+        talk(`Quote #${res.rows[0].id} by ${res.rows[0].author}:`, channel);
+        talk(res.rows[0].quote, channel);
+      });
+    }
+  });
+}
+
+const getNumQuotes = () => {
+    pgClient.query(`select count(*) from quotes`).then(r => {
+      numQuotes = r.rows[0].count;
+    });
 }
 
 bot.once('ready', () => {
   console.log(`DEBUG: Ready!`);
+  numQuotes = getNumQuotes();
+  console.log(numQuotes);
 });
 
-bot.on("message", (message) => {
-  if (message.author.tag != "Tweetster#1823") randomMessageCheck(message);
-  let msg = message.content;
+bot.on("message", (msg) => {
+  if (msg.author.tag != "Tweetster#1823") randomMessageCheck(msg);
+  const args = msg.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
   //admin command block
-  if (message.author.tag == "shaggers#3237") {
-    console.log(msg);
-    if (msg.startsWith(`${prefix}tweetChance`)) {
-      randomMessageChance = msg.split(" ")[1];
-      message.channel.send(`Tweeters set to ${randomMessageChance}%!`);
+  if (msg.author.tag == "shaggers#3237") {
+    if (command === 'tweet') {
+      if (!args[0]) msg.channel.send(`Tweeters are currently set to ${randomMessageChance}%! :]]`);
+      else {
+      randomMessageChance = args[0];
+      msg.channel.send(`Tweeters set to ${randomMessageChance}%!`);
+      }
     }
   }
   //normal command block
+  switch (command) {
+  case 'addquote':
+    if (!args[0]) talk('USAGE: !addquote <text>', msg);
+    else {
+      let author = msg.author.username;
+      let quote = args.join(' ');
+      addQuote(author, quote, msg);
+    }
+    break;
+  case 'getquote':
+    if (!args[0]) { talk('USAGE: !getquote <number>', msg); return; }
+    else if (isNaN(args[0])) talk('u big dunce thats not a number', msg);
+    else {
+      getQuote(args[0], msg);
+    }
+  break;
+  case 'numquotes':
+    getNumQuotes();
+    talk(`${numQuotes} total quote(s) in the database dunce.`, msg);
+    break;
+  default:
+  }
 });
  
 bot.login(token);
