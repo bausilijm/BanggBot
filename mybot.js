c8770677bffef0e446573cb686c5cf8133984e9b
Justin Bausili <bausilijm@gmail.com>
2021-02-17 19:39:49 -0500

    update

--- mybot.js
+++ mybot.js
@@ -1,13 +1,32 @@
-require('dotenv').config();
+const { prefix, token, randomMessages } = require('./config.json');
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-const token = process.env.token;
+var randomMessageChance = 100; //%
  
- 
-bot.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
+const randomMessageCheck = (message) => {
+  let random = Math.floor(Math.random() * 100);
+  if (random <= randomMessageChance) {
+    let msgNo = Math.floor(Math.random() * randomMessages.length);
+    message.channel.send(`${randomMessages[msgNo]}`);
   }
+}
+
+bot.once('ready', () => {
+  console.log(`DEBUG: Ready!`);
+});
+
+bot.on("message", (message) => {
+  if (message.author.tag != "Tweetster#1823") randomMessageCheck(message);
+  let msg = message.content;
+  //admin command block
+  if (message.author.tag == "shaggers#3237") {
+    console.log(msg);
+    if (msg.startsWith(`${prefix}tweetChance`)) {
+      randomMessageChance = msg.split(" ")[1];
+      message.channel.send(`Tweeters set to ${randomMessageChance}%!`);
+    }
+  }
+  //normal command block
 });
  
 bot.login(token);
